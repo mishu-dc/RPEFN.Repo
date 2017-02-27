db8f71d2d53736d3e4a41e037f5b5a507dcdc3bb
mishu <mishu_dc@yahoo.com>
2017-02-26 17:29:24 -0800

    data caching using MemoryCache

--- RPEFN.WebService/Controllers/DrugController.cs
+++ RPEFN.WebService/Controllers/DrugController.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Mvc;
+using System.Web.UI;
 using RPEFN.Data.Entities;
 using RPEFN.WebService.Dtos;
 using RPEFN.WebService.Infrastructure.Implementations;
 
 namespace RPEFN.WebService.Controllers
 {
-    [Authorize]
+    [System.Web.Http.Authorize]
     public class DrugController : ApiController
     {
         private readonly UnitOfWork _unitOfWork = null;
         private readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private const string DrugCachekey = "DRUG";
 
         public DrugController(UnitOfWork unitOfWork)
         {
@@ -21,12 +26,28 @@ namespace RPEFN.WebService.Controllers
             _logger.Info("Drug Controller Initialized successfully");
         }
 
-        [HttpGet]
+        private void InvalidateCache()
+        {
+            MemoryCache.Default.Remove(DrugCachekey);
+        }
+
+        [System.Web.Http.HttpGet]
         public async Task<IHttpActionResult> GetDrugsAsync()
         {
             try
             {
-                var drugs = await _unitOfWork.Drugs.GetAsync();
+                IEnumerable<Drug> drugs = null;
+
+                if (MemoryCache.Default[DrugCachekey] != null)
+                {
+                    drugs = (IEnumerable<Drug>) MemoryCache.Default[DrugCachekey];
+                }
+                else
+                {
+                    drugs = await _unitOfWork.Drugs.GetAsync();
+                    MemoryCache.Default[DrugCachekey] = drugs;
+                }
+
                 return Ok(drugs.Select(d => new DrugDto()
                 {
                     Id = d.Id,
@@ -48,7 +69,7 @@ namespace RPEFN.WebService.Controllers
             }
         }
 
-        [HttpGet]
+        [System.Web.Http.HttpGet]
         public async Task<IHttpActionResult> GetDrugAsync(int drugId)
         {
             try
@@ -82,7 +103,7 @@ namespace RPEFN.WebService.Controllers
 
         }
 
-        [HttpPost]
+        [System.Web.Http.HttpPost]
         public async Task<IHttpActionResult> CreateDrugAsync(DrugDto drug)
         {
             try
@@ -106,6 +127,8 @@ namespace RPEFN.WebService.Controllers
 
                     _logger.Info($"New drug created with id {drug.Id}");
 
+                    InvalidateCache();
+
                     return Created(new Uri(Request.RequestUri + "/" + drug.Id), drug);
                 }
                 else
@@ -125,7 +148,7 @@ namespace RPEFN.WebService.Controllers
             }
         }
 
-        [HttpPut]
+        [System.Web.Http.HttpPut]
         public async Task<IHttpActionResult> UpdateDrugAsync(DrugDto drug)
         {
             try
@@ -147,6 +170,8 @@ namespace RPEFN.WebService.Controllers
 
                     await _unitOfWork.CompleteAsync();
 
+                    InvalidateCache();
+
                     return Ok(drug);
                 }
                 else
@@ -166,7 +191,7 @@ namespace RPEFN.WebService.Controllers
             }
         }
 
-        [HttpDelete]
+        [System.Web.Http.HttpDelete]
         public async Task<IHttpActionResult> RemoveDrugAsync(int id)
         {
             try
@@ -179,6 +204,9 @@ namespace RPEFN.WebService.Controllers
                 }
                 _unitOfWork.Drugs.Remove(dbDrug);
                 await _unitOfWork.CompleteAsync();
+
+                InvalidateCache();
+
                 return Ok();
             }
             catch (Exception ex)
